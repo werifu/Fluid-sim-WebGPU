e8e9f5b7366d5efaefc08425647c15149e0ca67c
werifu <wohu1363195380@outlook.com>
2023-11-27 22:11:31 +0800

    fix: support ios

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,41 @@ import { setupGUI } from "./gui";
 import { Scene } from "./scene";
 import { createShader, meshFragmentShader, meshVertexShader, pointFragmentShader, pointVertexShader } from "./shaders";
 
+
+function permissionIOS(scene: Scene, acl: Acceleration) {
+  if (typeof (DeviceMotionEvent) !== "undefined" && typeof ((DeviceMotionEvent as any).requestPermission) === "function") {
+    // (optional) Do something before API request prompt.
+    (DeviceMotionEvent as any).requestPermission()
+      .then((response: any) => {
+        // (optional) Do something after API prompt dismissed.
+        if (response === "granted") {
+          window.addEventListener("devicemotion", (event: DeviceMotionEvent) => {
+            acl.x = event.accelerationIncludingGravity?.x!;
+            acl.y = event.accelerationIncludingGravity?.y!;
+            const screenAngle = window.orientation ?? 0;
+            if (screenAngle == 0) {
+              scene.gravity = {
+                x: acl.x,
+                y: acl.y
+              };
+            } else if (screenAngle === -90) {
+              scene.gravity = {
+                x: acl.y,
+                y: -acl.x,
+              }
+            } else if (screenAngle == 90) {
+              scene.gravity = {
+                x: -acl.y,
+                y: acl.x,
+              }
+            }
+          })
+        }
+      })
+      .catch(console.error)
+  }
+}
+
 async function main() {
   // window.scrollTo(0, 1);
   // const app = document.querySelector<HTMLDivElement>('#app')!;
@@ -105,46 +140,89 @@ async function main() {
   }
 
   // phone orientation changed
-  screen.orientation.onchange = (_: Event) => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  if (screen.orientation) {
+    screen.orientation.onchange = (_: Event) => {
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
 
-    if (screen.orientation.angle === 90 || screen.orientation.angle === 270) {
-      simWidth = 4.0;
-      const cScale = canvas.width / simWidth;
-      simHeight = canvas.height / cScale;
-    } else {
-      simHeight = 4.0;
-      const cScale = canvas.height / simHeight;
-      simWidth = canvas.width / cScale;
-    }
-    scene.setupScene({ simWidth, simHeight, density: 1000 });
-  };
+      if (screen.orientation.angle === 90 || screen.orientation.angle === 270) {
+        simWidth = 4.0;
+        const cScale = canvas.width / simWidth;
+        simHeight = canvas.height / cScale;
+      } else {
+        simHeight = 4.0;
+        const cScale = canvas.height / simHeight;
+        simWidth = canvas.width / cScale;
+      }
+      scene.setupScene({ simWidth, simHeight, density: 1000 });
+    };
+  } else if (window.orientation !== undefined) {
+    window.addEventListener("resize", () => {
+      // Get screen size (inner/outerWidth, inner/outerHeight)
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+
+      if (window.orientation === 90 || window.orientation === -90) {
+        simWidth = 4.0;
+        const cScale = canvas.width / simWidth;
+        simHeight = canvas.height / cScale;
+      } else {
+        simHeight = 4.0;
+        const cScale = canvas.height / simHeight;
+        simWidth = canvas.width / cScale;
+      }
+      scene.setupScene({ simWidth, simHeight, density: 1000 });
+    }, false);
+  }
+
 
   let acl: Acceleration = scene.gravity;
-  window.addEventListener('devicemotion', (event: DeviceMotionEvent) => {
-    acl.x = event.accelerationIncludingGravity?.x!;
-    acl.y = event.accelerationIncludingGravity?.y!;
-    const screenAngle = screen.orientation.angle;
-    // app.innerText = `aclX: ${acl.x}\naclY:${acl.y}\n`;
-    if (screenAngle == 0) {
-      scene.gravity = {
-        x: -acl.x,
-        y: -acl.y
-      };
-    } else if (screenAngle == 270) {
-      scene.gravity = {
-        x: -acl.y,
-        y: acl.x,
-      }
-    } else if (screenAngle == 90) {
-      scene.gravity = {
-        x: acl.y,
-        y: -acl.x,
-      }
-    }
 
-  })
+  // for ios devices
+  if (/iPad|iPhone|Macintosh/.test(navigator.userAgent)) {
+    var button = document.createElement('button');
+    button.innerHTML = 'iOS Click Me!';
+
+    // Style the button
+    button.style.position = 'fixed';
+    button.style.fontSize = '30px';
+    button.style.top = '50%';
+    button.style.left = '50%';
+    button.style.transform = 'translate(-50%, -50%)';
+
+    // Add the button to the document body
+    document.body.appendChild(button);
+
+    // Handle the button click event
+    button.addEventListener('click', () => {
+      permissionIOS(scene, acl);
+      button.remove();
+    });
+  } else {
+    window.addEventListener('devicemotion', (event: DeviceMotionEvent) => {
+      acl.x = event.accelerationIncludingGravity?.x!;
+      acl.y = event.accelerationIncludingGravity?.y!;
+      console.log(JSON.stringify(event));
+      const screenAngle = screen.orientation.angle ?? 0;
+      // app.innerText = `aclX: ${acl.x}\naclY:${acl.y}\n`;
+      if (screenAngle == 0) {
+        scene.gravity = {
+          x: -acl.x,
+          y: -acl.y
+        };
+      } else if (screenAngle == 270) {
+        scene.gravity = {
+          x: -acl.y,
+          y: acl.x,
+        }
+      } else if (screenAngle == 90) {
+        scene.gravity = {
+          x: acl.y,
+          y: -acl.x,
+        }
+      }
+    })
+  }
 
   scene.setupScene({
     simWidth, simHeight, density: 1000
